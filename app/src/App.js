11bdd79364f5b89d0c7ed5ca907e9ce410b5adcc
Val <valmack@protonmail.com>
2020-03-21 16:36:32 -0400

    Add wasm to react

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [wasmGame, setWasmGame] = useState(null);
+
+  useEffect(loadWasmGame, []);
+
+  async function loadWasmGame() {
+    try {
+      setLoading(true);
+      const wasm = await import('game');
+      setWasmGame(wasm);
+    } catch (err) {
+      setError(err);
+    }
+    setLoading(false);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Fast Coder</p>
+        {loading && <Loader />}
+        {error && <Error error={error} />}
+        {wasmGame && <Game wasm={wasmGame} />}
       </header>
     </div>
   );
-}
+};
+
+function Loader() {
+  return <div>Loading...</div>;
+};
+
+function Error() {
+  return <div>Error :(</div>;
+};
+
+function Game(props) {
+  return <div onClick={props.wasm.greet}>Click me!</div>;
+};
 
 export default App;
