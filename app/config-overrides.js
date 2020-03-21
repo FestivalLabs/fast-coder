b17174730694d52e990dc96c7cdc2cc249eb6776
Val <valmack@protonmail.com>
2020-03-21 16:18:54 -0400

    Configure react app for wasm

---
+++ app/config-overrides.js
@@ -0,0 +1,28 @@
+/*
+ * https://github.com/ballercat/wasm-loader/issues/3#issuecomment-459944069
+ */
+
+
+const path = require('path');
+
+module.exports = function overfide(config, env) {
+  const wasmExtensionRegExp = /\.wasm$/;
+
+  config.resolve.extensions.push('.wasm');
+
+  config.module.rules.forEach(rule => {
+    (rule.oneOf || []).forEach(oneOf => {
+      if (oneOf.loader && oneOf.loader.indexOf('file-loader') >= 0) {
+        oneOf.exclude.push(wasmExtensionRegExp);
+      }
+    });
+  });
+
+  config.module.rules.push({
+    test: wasmExtensionRegExp,
+    include: path.resolve(__dirname, 'src'),
+    use: [{ loader: require.resolve('wasm-loader'), options: {} }],
+  });
+
+  return config;
+};
